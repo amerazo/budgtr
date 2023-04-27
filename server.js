5816a6bc2d00e7936ab452dca304b2c8537dc48d
secretivo <secretivo@secretivos-MacBook-Air.local>
2023-04-26 21:53:03 -0700

    homework start

---
+++ server.js
@@ -0,0 +1,9 @@
+//Dependencies
+const express = require('express');
+const app = express();
+
+
+//Port Listened To
+app.listen(3000, () => {
+    console.log (Server is listening to PORT 3000);
+    })
